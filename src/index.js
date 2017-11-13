53701945484e3f8c474317173c8e63897329bda6
Tkac Anatoliy <atkach@ambellsolutions.com>
2017-11-13 23:32:46 +0200

    Add project

---
+++ src/index.js
@@ -0,0 +1,6 @@
+console.log(1 + 1);
+console.log('Hello');
+
+var h1 = document.createElement('h1');
+h1.innerHTML = 'Opa';
+document.body.appendChild(h1);
---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+const path = require('path');
+const HtmlPlugin = require('html-webpack-plugin');
+
+
+module.exports = {
+    context: path.join(__dirname, 'src'),
+    entry: './index',
+    output: {
+        path: path.join(__dirname, 'dist'),
+        filename: 'bundle.js'
+    },
+
+    plugins: [
+        new HtmlPlugin({
+            title: 'Webpack dev server'
+        })
+    ],
+
+    plugins: [
+        new HtmlPlugin({
+            title: 'Webpack dev server'
+        })
+    ]
+};
